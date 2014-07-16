1553fcb86173aaf96d3c1c073b9b74acb6ccd1e2
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-07-16 09:48:55 +0300

    Add missing IP config for OpenShift

--- serve.js
+++ serve.js
@@ -4,6 +4,6 @@ var app = express();
 
 // Spin up a static server
 app.use(express.static(config.dirs.feeds + '/feeds'));
-app.listen(config.port || 8080);
+app.listen(config.port || 8080, config.ip || '127.0.0.1');
 
 require('./generate.js');
