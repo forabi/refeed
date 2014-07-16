0f84996baaf48019f45da1b807da6ff832b8a893
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-07-16 11:18:04 +0300

    Use /feeds route for static xml files

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 // Spin up a static server
-app.use(express.static(path.join(config.dirs.root, config.dirs.feeds)));
+app.use('/feeds', express.static(path.join(config.dirs.root, config.dirs.feeds)));
 
 app.listen(config.port, config.ip, function() {
 	console.log('Server listening on ' + config.ip + ':' + config.port);
