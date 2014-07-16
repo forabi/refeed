b1bd154c8f8619291b30a31a5eb75cbf9cc4a6ce
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-07-16 09:09:45 +0300

    Fix main entry in package.json

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    port: process.env.OPENSHIFT_NODEJS_PORT,
-    ip: process.env.OPENSHIFT_NODEJS_IP,
-    dirs: {
-        feeds: OPENSHIFT_DATA_DIR
+    "port": process.env.OPENSHIFT_NODEJS_PORT,
+    "ip": process.env.OPENSHIFT_NODEJS_IP,
+    "dirs": {
+        "feeds": OPENSHIFT_DATA_DIR
     }
 }
