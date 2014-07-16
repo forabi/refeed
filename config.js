5ecf2e65523ed2885d9ad6d7c782603146d31313
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-07-16 09:26:31 +0300

    Fix OPENSHIFT_DATA_DIR namespace

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     "port": process.env.OPENSHIFT_NODEJS_PORT,
     "ip": process.env.OPENSHIFT_NODEJS_IP,
     "dirs": {
-        "feeds": OPENSHIFT_DATA_DIR
+        "feeds": process.env.OPENSHIFT_DATA_DIR
     }
 }
