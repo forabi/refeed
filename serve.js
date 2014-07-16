ad8f40e27f67a1a5028bba8037613e2c541527e9
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-07-16 08:20:28 +0300

    Move to OpenShift, create a config file

--- serve.js
+++ serve.js
@@ -1,8 +1,9 @@
+var config = require('./config.js');
 var express = require('express');
 var app = express();
 
 // Spin up a static server
-app.use(express.static(__dirname + '/feeds'));
-app.listen(process.env.PORT || 3000);
+app.use(express.static(config.dirs.feeds + '/feeds'));
+app.listen(config.port || 3000);
 
 require('./generate.js');
